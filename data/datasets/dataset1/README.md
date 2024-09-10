7d2f3e13c38e850342ce9a6d16c130dac0ffae8f
Adrián Ayuso-Muñoz <180382013+amadrian00@users.noreply.github.com>
2024-09-10 17:15:24 +0900

    Open dataset code

---
+++ data/datasets/dataset1/README.md
@@ -0,0 +1,3 @@
+High-quality diffusion-weighted imaging of Parkinson's disease
+https://github.com/CyclotronResearchCentre/parktdi_scripts
+https://www.nitrc.org/frs/?group_id=835
--- data/prepareDataset.py
+++ data/prepareDataset.py
@@ -1,14 +1,24 @@
 """
 Adrián Ayuso Muñoz 2024-09-09 for the GNN-MRI project.
 """
+import os
+import sys
+import pandas as pd
 
 """ Input:  dataset_name: List of string that indicates dataset to be used.
     Output: Dataset instance.
 
     Function that prepares and returns the dataset."""
 def get_dataset(dataset_name):
-    dataset_path = get_dataset_path(dataset_name)
-    dataset = open(dataset_path)
+    dataset_path = './data/datasets/' + dataset_name
+    if not os.path.isdir(dataset_path):
+        print(f" {'\033[31m'} Error: The dataset '{dataset_path}' does not exist.")
+        sys.exit(1)  # Exit the script with a non-zero status indicating an error
+
+    dataset = generate_dataset(dataset_name, dataset_path)
+
+    dataset = 0
+    #dataset = open(dataset_path)
     return dataset
 
 """ Input:  dataset: Dataset instance.
@@ -21,15 +31,13 @@ def add_clusters(dataset, clusters):
     return dataset
 
 """ Input:  dataset_name: List of string that indicates dataset to be used.
-    Output: Dataset path.
+            dataset_path: Path to the dataset directory.
+    Output: Dataset ready to enter the pipeline.
 
-    Function that returns the dataset path."""
-def get_dataset_path(dataset_name):
-    if dataset_name == "a":
-        dataset_path = "./datasets/A/"
-    else:
-        dataset_path = "./datasets/B/"
-    return dataset_path
+    Function that returns the dataset."""
+def generate_dataset(dataset_name, dataset_path):
+    metadata = pd.read_csv(dataset_path+'/metadata.csv', usecols=["Subject", "Sex","Age"])
+    print(metadata)
 
 if __name__=="__main__":
     exit(0)
