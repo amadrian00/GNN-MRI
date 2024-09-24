23d535171017a6e6b82e1fb2a37e1fd762f62b28
Adrián Ayuso-Muñoz <180382013+amadrian00@users.noreply.github.com>
2024-09-24 20:26:34 +0900

    Refactor DataSet class

--- data/prepareDataset.py
+++ data/prepareDataset.py
@@ -5,36 +5,42 @@ import os
 import sys
 import pandas as pd
 
-""" Input:  dataset_name: List of string that indicates dataset to be used.
-    Output: Dataset instance.
+class DataSet:
+    def __init__(self,dataset_name):
+        self.dataset_name = dataset_name
+        self.dataset = None
 
-    Function that prepares and returns the dataset."""
-def get_dataset(dataset_name):
-    dataset_path = './data/datasets/' + dataset_name
-    if not os.path.isdir(dataset_path):
-        print(f" {'\033[31m'} Error: The dataset '{dataset_path}' does not exist.")
-        sys.exit(1)  # Exit the script with a non-zero status indicating an error
+    """ Input:  -
+        Output: Dataset instance.
+    
+        Function that prepares and returns the dataset."""
+    def get_dataset(self):
+        dataset_path = './data/datasets/' + self.dataset_name
+        if not os.path.isdir(dataset_path):
+            print(f" {'\033[31m'} Error: The dataset '{dataset_path}' does not exist.")
+            sys.exit(1)  # Exit the script with a non-zero status indicating an error
 
-    dataset = generate_dataset(dataset_path)
-    return dataset
+        self.dataset = self.generate_dataset(dataset_path)
+        return self.dataset
 
-""" Input:  dataset: Dataset instance.
-            clusters: Assigned clusters.
-    Output: Dataset instance.
+    """ Input:  dataset: Dataset instance.
+                clusters: Assigned clusters.
+        Output: Dataset instance.
+    
+        Function that appends the assigned cluster to each element."""
+    @staticmethod
+    def add_clusters(dataset, clusters):
+        dataset.append(clusters)
+        return dataset
 
-    Function that appends the assigned cluster to each element."""
-def add_clusters(dataset, clusters):
-    dataset.append(clusters)
-    return dataset
-
-""" Input:  dataset_path: Path to the dataset directory.
-    Output: Dataset ready to enter the pipeline.
-
-    Function that returns the dataset."""
-def generate_dataset(dataset_path):
-    metadata = pd.read_csv(dataset_path+'/metadata.csv', usecols=["Subject", "Sex","Age"])
-
-    return metadata
+    """ Input:  dataset_path: String that indicates root path to dataset
+        Output: Dataset ready to enter the pipeline.
+    
+        Function that returns the dataset with the labels."""
+    @staticmethod
+    def generate_dataset(dataset_path):
+        dataset_and_labels = pd.read_csv(dataset_path+'/metadata.csv', usecols=["Subject", "Sex","Age"])
+        return dataset_and_labels
 
 if __name__=="__main__":
     exit(0)
