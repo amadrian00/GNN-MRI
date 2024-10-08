6f574affb95cb49f5a31d123505fd5479d67775f
Adrián Ayuso-Muñoz <180382013+amadrian00@users.noreply.github.com>
2024-10-08 17:05:17 +0900

    Add .idea and test (Remove in final version)

---
+++ test.py
@@ -0,0 +1,20 @@
+import os
+import torch
+from torch.utils import data
+from data import prepareDataset
+from brainEncoder import brainEncoder
+
+if __name__ == '__main__':
+    os.environ["CUDA_VISIBLE_DEVICES"] = "4"
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    print('Executing on:', torch.cuda.get_device_name())
+
+    batch_size = 16
+
+    dataset = prepareDataset.DallasDataSet(device)
+    dataloader = data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    brainEncoder = brainEncoder.BrainEncoder(device, dataset.__getitem__(0),'AutoEncoder')
+
+    brainEncoder.train(dataloader, batch_size)
